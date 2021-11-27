2b7003d1c7aa8ca14e0899b2a579866ab54915e4
James Petran <jm.ptrn@gmail.com>
2021-11-26 21:35:46 -0600

    rename logC to showCollection & add tests

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,18 +12,27 @@ function addToCollection( title1, artist1, yearPublished1 ) {
     return record1;
 } //end addToCollection
 
-function logC(collection1) { //I'm adding this because I want to call this rather than do console.log() over and over again
-    console.log(`collection is`);
-    for (const album of collection1) {
-        console.log(`$album`);
+function showCollection(collection1) { 
+    console.log(`collection contains ${collection.length} items`);
+    if (collection1.length < 1) {
+        return null
     }
+    for (const album of collection1) {
+        console.log(album);
+    }
+
     
     return null
 } //end logCollection 
 
-logC();
+showCollection(collection); //empty
 console.log(`adding Avicii's True, 2013` , addToCollection(`True`, `Avicii`, 2013) );
-logC();
+showCollection(collection); //True by avicii, 2013
+console.log(`adding Avicii's Stories, 2015` , addToCollection(`Stories`, `Avicii`, 2015) );
+console.log(`adding Avicii's Tim, 2019 (RIP)` , addToCollection(`Tim`, `Avicii`, 2019) );
+console.log(`adding Kygo's Cloud Nine, 2016` , addToCollection(`Cloud Nine`, `Kygo`, 2016) );
+console.log(`adding Kygo's Kids in Love, 2017` , addToCollection(`Kids in Love`, `Kygo`, 2017) );
+console.log(`adding Kygo's Golden Hour, 2020` , addToCollection(`Golden Hour`, `Kygo`, 2020) );
 
 
 
